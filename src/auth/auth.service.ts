a0140584eac9d8ce33b33caffe743de411a4aa8f
Felipe Carneiro <correaflp@gmail.com>
2024-03-30 18:00:28 -0300

    Refactor auth.service.ts to handle duplicate user creation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClient } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -11,21 +16,33 @@ export class AuthService {
     // Hash the password
     const hash = await argon.hash(dto.password);
     // Save the user to the database
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    delete user.hash;
-    
-    // Return the user
-    return user;
-    return {
-      message: 'Hello World! I am signing up...',
-    };
+      delete user.hash;
+
+      // Return the user
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'User with this email already exists.',
+          );
+        }
+      }
+      throw error;
+    }
   }
+
   signin() {
     return {
       message: 'Hello World! I am signing in...',
