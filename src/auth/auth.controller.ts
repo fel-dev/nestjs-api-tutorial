f37df6aa1e5954b5dbb19f86d4f7011b09c42644
Felipe Carneiro <correaflp@gmail.com>
2024-03-30 14:46:26 -0300

    Update .prettierrc and auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,30 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(
+    @Body('email') email: string,
+    @Body('password', ParseIntPipe) password: string,
+  ) {
+    console.log({
+      email,
+      typeOfEmail: typeof email,
+      password,
+      typeOfPassword: typeof password,
+    });
     return this.authService.signup();
   }
+
   // request POST http://localhost:3000/auth/signin
   @Post('signin')
   signin() {
